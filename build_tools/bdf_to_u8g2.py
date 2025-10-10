76284b79763975b175f3e3a54955e8f81239b4e4
Dan Egnor <egnor@ofb.net>
2025-10-10 01:09:42 -0700

    u8g2 binaries

--- build_tools/bdf_to_u8g2.py
+++ build_tools/bdf_to_u8g2.py
@@ -16,24 +16,17 @@ try: top_dir = top_dir.relative_to(Path.cwd())
 except ValueError: pass
 
 conversions_dir = top_dir / "derived_bdfs"
-bdf_paths = list(sorted(conversions_dir.glob("**/*.bdf")))
-if not bdf_paths:
-    ok_logging_setup.exit(f"No .bdf files in {conversions_dir}")
+bdf_paths = list(sorted(conversions_dir.glob("*.bdf")))
+if not bdf_paths: ok_logging_setup.exit(f"Missing: {conversions_dir}/*.bdf")
 
 subproc = ok_subprocess_defaults.SubprocessDefaults()
 arch = f"{platform.machine().lower()}-{platform.system().lower()}"
 bdfconv_path = top_dir / "build_tools" / f"{arch}-bdfconv.exe"
-if not bdfconv_path.is_file():
-    ok_logging_setup.exit(f"Missing: {bdfconv_path}")
+if not bdfconv_path.is_file(): ok_logging_setup.exit(f"Missing: {bdfconv_path}")
 
 src_dir = top_dir / "src"
-src_dir.mkdir(parents=True, exist_ok=True)
-
-delete_paths = set()
-delete_paths.update(src_dir.glob("font_*.c"))
-for delete_path in sorted(delete_paths):
-    logging.info(f"🗑️ Deleting: {delete_paths}")
-    delete_path.unlink()
+logging.info(f"🗑️ Cleaning: {src_dir}/font_*.c")
+[p.unlink() for p in src_dir.glob("font_*.c")]
 
 for bdf_path in bdf_paths:
     logging.info(f"\n🔠 Converting: {bdf_path}")
@@ -45,4 +38,4 @@ for bdf_path in bdf_paths:
         f"-n{font_name}", f"-o{font_path}", bdf_path,
     )
 
-logging.info(f"\n🏁 Done converting {len(bdf_paths)} .bdf fonts to u8g2")
+logging.info(f"\n🏁 Updated {len(bdf_paths)} u8g2 font files")
